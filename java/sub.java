33f801023b599b35d52cbe0700181a06d00865e9
elayabharathi-me18 <elayabharathi.me18@bitsathy.ac.in>
2021-10-11 15:25:19 +0000

    11/10/2021

--- java/sub.java
+++ java/sub.java
@@ -9,7 +9,7 @@ class sub
 		System.out.println("constant: " + c);
 		System.out.println("Integer : " + (Integer.parseInt(args[0])-Integer.parseInt(args[1])));
 		System.out.println("Float : " + (Float.parseFloat(args[2])-Float.parseFloat(args[3])));
-		System.out.println(d+" "+e);
+		System.out.println(d+" "+e+"=");
 		System.out.println(d-e);
 	}
 }
