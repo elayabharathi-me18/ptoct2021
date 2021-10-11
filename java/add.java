fa6486007cadeb67c29d5f249970322cf7cdd65b
elayabharathi-me18 <elayabharathi.me18@bitsathy.ac.in>
2021-10-11 10:08:01 +0000

    addition

--- java/add.java
+++ java/add.java
@@ -2,7 +2,16 @@ class add
 {
 	public static void main(String []args)
 	{
-		int a=10,b=20;
-		System.out.println(a+b);
-	}
+		int a=10,b=20,c;
+		c=a+b;
+		int d=Integer.parseInt(args[0])+Integer.parseInt(args[1]);
+		float e=Float.parseFloat(args[2])+Float.parseFloat(args[3]);
+		System.out.println("constant: " + c);
+		System.out.println("Integer: " + (Integer.parseInt(args[0])+Integer.parseInt(args[1])));
+		System.out.println("Float: " + (Float.parseFloat(args[2])+Float.parseFloat(args[3])));
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(d+" "+e);
+		System.out.println(d+e);	
+}
 }
