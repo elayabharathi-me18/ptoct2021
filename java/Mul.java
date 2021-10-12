99f6d7f30cad45761617a4e869c4eea24fb6d69c
elayabharathi-me18 <elayabharathi.me18@bitsathy.ac.in>
2021-10-12 05:10:48 +0000

    12/10

---
+++ java/Mul.java
@@ -0,0 +1,10 @@
+class Mul
+{
+	public static void main(String []args)
+	{	
+		int a=5,b=8;
+		System.out.println("constant: " + a*b);
+		System.out.println("Integer : " + (Integer.parseInt(args[0])*Integer.parseInt(args[1])));
+		
+	}
+}
